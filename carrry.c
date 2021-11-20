617a61f755e2916dfbae220d3a41d1817387fb9f
Pankaj Bedre <66171783+pankajbedre@users.noreply.github.com>
2021-11-20 10:43:54 +0530

    Update carrry.c

--- carrry.c
+++ carrry.c
@@ -3,20 +3,22 @@
 
 int main()
 {
-   int a, b, carry=0, newA, newB, check=1;
+   int a, b, carry=0, carN = 0, check=1;
    
    scanf("%d", &a);
    scanf("%d", &b);
    
    while(check) {
-       if((a%10+b%10)>9) {
+       carN = (a%10+b%10)/10;
+       if((a%10+b%10+carN)>9) {
            carry++;
        }
-       a = a/10;
-       b = b/10;
+       a = a/10 + carN;
+       b = b/10 ;
        if(a<=0||b<=0){
            check = 0;
        }
+       carN = 0;
    }
    if(carry){
        printf("%d carry opretions", carry);
